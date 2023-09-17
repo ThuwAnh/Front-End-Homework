a1bd2b466ff43196b23ef1dc5bdaa66860508be7
LAP01745 <lap01745@LAP01745.local>
2023-09-17 20:32:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2EC5CE',
+        'secondary': '#D5FAFC',
+      },
+      fontSize: {
+        h1: ["72px", "98px"],
+        h2: ["48px", "64px"],
+        h3: ["40px", "54px"],
+        h5: ["24px", "32px"],
+        body: ["16px", "24px"],
+        lead1: ["18px", "32px"],
+        caption: ["14px", "28px"],
+      },
+      fontFamily: {
+        inter: "Inter",
+      }
+    },
+  },
+  plugins: [],
+}
